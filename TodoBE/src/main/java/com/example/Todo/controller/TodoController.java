ccd0ad57ce73f34be8c44f67123c9dc0ee074550
HONG-EUN-YOUNG <dms084@outlook.com>
2025-02-09 23:12:20 +0900

    feat: Implement CRUD for Todo API
    ### 2.3 서비스 개발 및 실습
    - TodoController, TodoDTO, TodoService에 CRUD 기능 추가
    - userId를 이용한 Todo 목록 조회
    - userId는 임시로 고정값 박아놔서 4장에서 수정 예정

--- TodoBE/src/main/java/com/example/Todo/controller/TodoController.java
+++ TodoBE/src/main/java/com/example/Todo/controller/TodoController.java
@@ -1,14 +1,15 @@
 package com.example.Todo.controller;
 
 import com.example.Todo.dto.ResponseDTO;
+import com.example.Todo.dto.TodoDTO;
+import com.example.Todo.model.TodoEntity;
 import com.example.Todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -25,5 +26,102 @@ public class TodoController {
 
     ResponseDTO<String> res = ResponseDTO.<String>builder().data(list).build();
     return ResponseEntity.ok().body(res);
+  }
+
+  // Todo 아이템 생성
+  @PostMapping
+  public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+    try {
+      // ### 임시 유저 아이디 -> 4장에서 수정
+      String temporaryUserId = "temporary-user";
+
+      // Dto -> TodoEntity
+      TodoEntity entity = TodoDTO.toEntity(dto);
+      entity.setId(null);
+      entity.setUserId(temporaryUserId);  // ### 4장에서 수정
+      
+      // 엔티티 생성
+      List<TodoEntity> entities = service.create(entity);
+
+      // 자바 스트림으로 Entity -> Dto
+      List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+      ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+      return ResponseEntity.ok().body(response);
+    }
+    catch (Exception e) {
+      // 예외는 dto 대신 error에 메시지 넣어 리턴
+      String error = e.getMessage();
+      ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+      return ResponseEntity.badRequest().body(response);
+    }
+  }
+
+  // Todo 목록 검색
+  @GetMapping
+  public ResponseEntity<?> searchTodoList() {
+    // ### 임시 유저 아이디 -> 4장에서 수정
+    String temporaryUserId = "temporary-user";
+
+    // 유저의 Todo 검색
+    List<TodoEntity> entities = service.search(temporaryUserId);
+
+    // 자바 스트림으로 Entity -> Dto
+    List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+    ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+    return ResponseEntity.ok().body(response);
+  }
+
+  // Todo 아이템 수정
+  @PutMapping
+  public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
+    // ### 임시 유저 아이디 -> 4장에서 수정
+    String temporaryUserId = "temporary-user";
+
+    // Dto -> TodoEntity
+    TodoEntity entity = TodoDTO.toEntity(dto);
+    entity.setUserId(temporaryUserId);  // ### 4장에서 수정
+    
+    // 수정
+    List<TodoEntity> entities = service.update(entity);
+
+    // 자바 스트림으로 Entity -> Dto
+    List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+    ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+    return ResponseEntity.ok().body(response);
+  }
+
+  // Todo 아이템 삭제
+  @DeleteMapping
+  public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto) {
+    try {
+      // ### 임시 유저 아이디 -> 4장에서 수정
+      String temporaryUserId = "temporary-user";
+
+      // Dto -> TodoEntity
+      TodoEntity entity = TodoDTO.toEntity(dto);
+      entity.setUserId(temporaryUserId);  // ### 4장에서 수정
+      
+      // 삭제
+      List<TodoEntity> entities = service.delete(entity);
+
+      // 자바 스트림으로 Entity -> Dto
+      List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+      ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+      return ResponseEntity.ok().body(response);
+    }
+    catch (Exception e) {
+      // 예외는 dto 대신 error에 메시지 넣어 리턴
+      String error = e.getMessage();
+      ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+      return ResponseEntity.badRequest().body(response);
+    }
   }
 }
--- TodoBE/src/main/java/com/example/Todo/service/TodoService.java
+++ TodoBE/src/main/java/com/example/Todo/service/TodoService.java
@@ -2,9 +2,13 @@ package com.example.Todo.service;
 
 import com.example.Todo.model.TodoEntity;
 import com.example.Todo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @Service
 public class TodoService {
   @Autowired
@@ -20,5 +24,89 @@ public class TodoService {
     return savedEntity.getTitle();
 
 //    return "Test Service";
+  }
+
+  // Todo 아이템 생성
+  public List<TodoEntity> create(final TodoEntity entity) {
+    // 데이터 검증
+    validate(entity);
+
+    repository.save(entity);
+    log.info("Entity Id : {} is saved", entity.getId());
+
+    return repository.findByUserId(entity.getUserId());
+  }
+
+  // 검증부분 리팩토링
+  private void validate(final TodoEntity entity) {
+    if (entity == null) {
+      log.warn("Entity cannot be null");
+      throw new RuntimeException("Entity cannot be null");
+    }
+
+    if (entity.getUserId() == null) {
+      log.warn("Unknown user");
+      throw new RuntimeException("Unknown user");
+    }
+  }
+
+  // Todo 목록 검색
+  public List<TodoEntity> search(final String userId) {
+    return repository.findByUserId(userId);
+  }
+
+  // Todo 아이템 수정 1 (Optional, Lambda 사용)
+  public List<TodoEntity> update(final TodoEntity entity) {
+    // 데이터 검증
+    validate(entity);
+
+    // 수정할 아이템 entity
+    final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+    // original가 존재하면 새 값으로 덮어쓰기
+    original.ifPresent(todo -> {
+      todo.setTitle(entity.getTitle());
+      todo.setDone(entity.isDone());
+      
+      repository.save(todo);
+    });
+
+    return search(entity.getUserId());
+  }
+
+  // Todo 아이템 수정 2 (if문 사용)
+//  public List<TodoEntity> update(final TodoEntity entity) {
+//    validate(entity);
+//
+//    final Optional<TodoEntity> original = repository.findById(entity.getId());
+//
+//    if(original.isPresent()) {
+//      final TodoEntity todo = original.get();
+//      todo.setTitle(entity.getTitle());
+//      todo.setDone(entity.isDone());
+//
+//      repository.save(todo);
+//    }
+//
+//    return retrieve(entity.getUserId());
+//  }
+  
+  // Todo 아이템 삭제
+  public List<TodoEntity> delete(final TodoEntity entity) {
+    // 데이터 검증
+    validate(entity);
+
+    try {
+      // 엔티티 삭제
+      repository.delete(entity);
+    }
+    catch(Exception e) {
+      log.error("error deleting entity ", entity.getId(), e);
+
+      // 캡슐화를 위해 e 대신 새 exception 리턴
+      throw new RuntimeException("error deleting entity " + entity.getId());
+    }
+
+    return search(entity.getUserId());
   }
 }
