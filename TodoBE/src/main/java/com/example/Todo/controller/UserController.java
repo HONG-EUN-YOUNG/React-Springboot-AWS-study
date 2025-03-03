5202f157feb786969c2f1b71ba2cf82c56d4f613
HONG-EUN-YOUNG <dms084@outlook.com>
2025-03-03 22:18:54 +0900

    feat: Password encryption (4.3)

--- TodoBE/src/main/java/com/example/Todo/controller/UserController.java
+++ TodoBE/src/main/java/com/example/Todo/controller/UserController.java
@@ -8,6 +8,8 @@ import com.example.Todo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,8 @@ public class UserController {
   @Autowired
   private TokenProvider tokenProvider;
 
+  private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
   // 회원가입
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
@@ -34,7 +38,7 @@ public class UserController {
       // 유저 정보 생성
       UserEntity user = UserEntity.builder()
           .username(userDTO.getUsername())
-          .password(userDTO.getPassword())
+          .password(passwordEncoder.encode(userDTO.getPassword()))
           .build();
 
       // 유저 저장
@@ -59,7 +63,8 @@ public class UserController {
   public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
     UserEntity user = userService.getByCredentials(
         userDTO.getUsername(),
-        userDTO.getPassword());
+        userDTO.getPassword(),
+        passwordEncoder);
 
     if(user != null) {
       // 토큰 생성
--- TodoBE/src/main/java/com/example/Todo/service/UserService.java
+++ TodoBE/src/main/java/com/example/Todo/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.Todo.model.UserEntity;
 import com.example.Todo.persistence.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -29,7 +30,14 @@ public class UserService {
   }
 
   // 로그인
-  public UserEntity getByCredentials(final String username, final String password) {
-    return userRepository.findByUsernameAndPassword(username, password);
+  public UserEntity getByCredentials(final String username, final String password,
+                                     final PasswordEncoder encoder) {
+    final UserEntity originalUser = userRepository.findByUsername(username);
+
+    if(originalUser != null &&
+        encoder.matches(password, originalUser.getPassword())) {
+      return originalUser;
+    }
+    return null;
   }
 }
