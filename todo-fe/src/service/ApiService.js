1bdcbe311f463f08ae09a14dad9b25d4b62b8baa
HONG-EUN-YOUNG <dms084@outlook.com>
2025-03-12 21:11:13 +0900

    feat: Implement token-based authentication (5.3)
    - 로그인 시 로컬 스토리지에 액세스 토큰 저장
    - 모든 API 요청 헤더에 액세스 토큰 자동 추가

--- todo-fe/src/service/ApiService.js
+++ todo-fe/src/service/ApiService.js
@@ -1,10 +1,19 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  // 헤더에 액세스 토큰 추가
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+
+  if (accessToken && accessToken !== null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
@@ -21,8 +30,7 @@ export function call(api, method, request) {
       } else if (response.status === 403) {
         window.location.href = "/login"; // 로그인 페이지로 redirect
       }else {
-        Promise.reject(response);
-        throw Error(response);
+        new Error(response);
       }
     })
     .catch((error) => {
@@ -35,8 +43,10 @@ export function call(api, method, request) {
 export function signin(userDTO) {
   return call("/auth/signin", "POST", userDTO)
   .then((response) => {
-    // token 있으면 todo화면으로 리디렉트
     if (response.token) {
+      // 로컬 스토리지에 토큰 저장
+      localStorage.setItem("ACCESS_TOKEN", response.token);
+      // token 있으면 todo화면으로 리디렉트
       window.location.href = "/";
     }
   });
