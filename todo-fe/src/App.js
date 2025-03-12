6679b8e50aa97e06cca7dd201dd1e117bcd18334
HONG-EUN-YOUNG <dms084@outlook.com>
2025-03-12 21:15:58 +0900

    feat: Implement logout functionality (5.4)
    - 로그아웃 시 로컬 스토리지에서 토큰 삭제
    - 네비게이션 바 및 로그아웃 버튼 추가
    - UI 글리치 해결을 위해 로딩 화면 적용

--- todo-fe/src/App.js
+++ todo-fe/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
-import { List, Paper, Container } from "@mui/material";
+import { List, Paper, Container, Grid,
+  Button, AppBar, Toolbar, Typography, } from "@mui/material";
 import Todo from "./Todo/Todo";
 import AddTodo from "./Todo/AddTodo"
-import { call } from "./service/ApiService";
+import { call, signout } from "./service/ApiService";
 
 
 function App() {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Todo 추가
   const addItem = (item) => {
@@ -30,7 +32,10 @@ function App() {
   // Todo API 호출
   useEffect(() => {
     call("/todo", "GET", null)
-      .then((response) => setItems(response.data));
+      .then((response) => {
+        setItems(response.data);
+        setLoading(false);
+    });
   }, []);
 
   let todoItems = items.length > 0 && (
@@ -45,12 +50,48 @@ function App() {
     </Paper>
   );
 
-  return (
-    <div className="App">
+  // 네비게이션 바
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">오늘의 할일</Typography>
+          </Grid>
+
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
+  // 로딩중 아닐 때 렌더링 할 부분
+  let todoListPage = (
+    <div>
+      {navigationBar}
+
       <Container maxWidth="md">
         <AddTodo addItem={addItem} />
         <div className="TodoList">{todoItems}</div>
       </Container>
+    </div>
+  );
+
+  // 로딩중일 때 렌더링 할 부분
+  let loadingPage = <h1> 로딩중... </h1>;
+  let content = loadingPage;
+
+  if (!loading) {
+    content = todoListPage;
+  }
+
+  return (
+    <div className="App">
+      {content}
     </div>
   );
 }
